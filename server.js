6a6baa1b5d86f0f47f5145dda6abdcc3cbdea077
Bharat Kashyap <bharatkashyap@outlook.com>
2016-11-05 01:40:27 +0530

    cleaning up

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/View'));
+app.use(express.static(__dirname + "/View"));
 
-app.use(express.static(__dirname + '/Script'));
+app.use(express.static(__dirname + "/Script"));
 
-app.use(express.static(__dirname + '/Data'));
+app.use(express.static(__dirname + "/Data"));
 
-app.use(express.static(__dirname + '/Media'))
+app.use(express.static(__dirname + "/Media"))
 
 
 app.get('/', function(req, res)
